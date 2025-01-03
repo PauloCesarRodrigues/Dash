21ea5e9c00426bad6e123ee22906abf1c4ba484a
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-01-03 05:34:10 -0300

    add: Auth page styles, dashboard Header and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './index.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
+  )
 }
